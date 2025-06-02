b14fb725087e5cbb0aae4611290439e01b43919c
Tanvi <tanvi@Tanvis-MacBook-Air.local>
2025-06-02 13:37:12 +1000

    Chnage socket name in db.js

--- db.js
+++ db.js
@@ -2,11 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  socketPath: `/cloudsql/${process.env.INSTANCE_UNIX_SOCKET}`,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
